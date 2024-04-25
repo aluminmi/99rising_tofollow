56cab82eb88f77081943cc7fddd9ad2d4c2878b9
Naomi Cuerdo <98798591+aluminmi@users.noreply.github.com>
2024-04-25 13:14:07 +0800

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">PORTFOLIO</a></li>
+        <li><a href="#">CONTACT</a></li>
+        <li><a href="#">BOOKINGS</a></li>
+    </ul>
+</nav>
