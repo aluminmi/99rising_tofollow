f5a54805f92278d2002d17d6ca2fcd1035b17095
mosta <mostafakhafaji2004@gmail.com>
2024-04-28 13:08:45 +0800

    fixed links for navi

--- navigation.php
+++ navigation.php
@@ -1,10 +1,10 @@
 <nav>
     <ul>
         <li><a href="home.php">HOME</a></li>
-        <li><a href="#">ABOUT</a></li>
-        <li><a href="#">SERVICES</a></li>
+        <li><a href="about.php">ABOUT</a></li>
+        <li><a href="nail-services.php">SERVICES</a></li>
         <li><a href="portfolio.php">PORTFOLIO</a></li>
-        <li><a href="#">CONTACT</a></li>
+        <li><a href="contact.php">CONTACT</a></li>
         <li><a href="#">BOOKINGS</a></li>
     </ul>
 </nav>
