408aed6e49ce6d8f8530e06b81a62d2fec3cff08
onjmm <124319835+onjmm@users.noreply.github.com>
2024-05-05 21:19:26 +0800

    Add files via upload

--- navigation.php
+++ navigation.php
@@ -2,7 +2,13 @@
     <ul>
         <li><a href="home.php">HOME</a></li>
         <li><a href="about.php">ABOUT</a></li>
-        <li><a href="services.php">SERVICES</a></li>
+        <li class="dropdown">
+            <a href="services.php" class="dropbtn">SERVICES</a>
+            <div class="dropdown-content">
+                <a href="hair-services.php">Hair Services</a>
+                <a href="nail-services.php">Nail Services</a>
+            </div>
+        </li>
         <li><a href="portfolio.php">PORTFOLIO</a></li>
         <li><a href="contact.php">CONTACT</a></li>
         <li><a href="#">BOOKINGS</a></li>
